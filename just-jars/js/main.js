0f15ed59ac134bf7a3c4821a2ac60e1f49c2dd88
sophieshron <sophieshron@gmail.com>
2020-05-11 10:23:52 -0400

    just jars 1

---
+++ just-jars/js/main.js
@@ -0,0 +1,16 @@
+var myName = 'sophie';
+
+console.log (myName);
+
+
+
+
+function setNewImage() {
+  document.getElementById("pickles").src ="img/sauce.svg";
+  document.getElementById("logo").src ="img/just-jars-logo-text-straight.svg";
+}
+
+function setOldImage() {
+  document.getElementById("pickles").src ="img/pickles.svg";
+  document.getElementById("logo").src ="img/just-jars-logo-text.svg";
+}
